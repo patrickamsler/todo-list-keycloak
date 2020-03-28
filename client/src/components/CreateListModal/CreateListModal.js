ac8928ad999df1ab4a9bc10cd50b289e5f9d50bd
Patrick Amsler <patrick.amsler@zuhlke.com>
2020-03-28 06:17:07 -0700

    Modal improved

--- client/src/components/CreateListModal/CreateListModal.js
+++ client/src/components/CreateListModal/CreateListModal.js
@@ -4,13 +4,19 @@ import { Button, Input, Modal } from 'semantic-ui-react'
 const CreateListModal = () => {
   
   const [modalState, setModalState] = useState({open: false});
+  const [listTitle, setListTitle] = useState('');
   
-  const close = () => {
+  const close = (create) => {
+    if (create) {
+      console.log(`Create list ${listTitle}`);
+    }
     setModalState({open: false});
+    setListTitle('');
   };
   
   const open = () => {
     setModalState({open: true});
+    setListTitle('');
   };
   
   return (
@@ -22,18 +28,23 @@ const CreateListModal = () => {
       >
         <Modal.Header>Create new Todo List</Modal.Header>
         <Modal.Content>
-          <Input style={{width: "100%"}} />
+          <Input
+              style={{width: "100%"}}
+              value={listTitle}
+              onChange={e => setListTitle(e.target.value)}
+          />
         </Modal.Content>
         <Modal.Actions>
           <Button
               secondary
-              onClick={close}
+              onClick={() => close(false)}
           >
             Cancel
           </Button>
           <Button
               primary
-              onClick={close}
+              onClick={() => close(true)}
+              disabled={listTitle.length === 0}
           >
             Create
           </Button>
